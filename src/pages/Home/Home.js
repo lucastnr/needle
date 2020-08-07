772b924b319a83d9951a5c1a0687b024b24a2f0a
rafa7046 <rafael-vilella@hotmail.com>
2020-08-07 20:44:00 -0300

    version 0.1.4

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
   const { logout } = useAuth()
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1,flexDirection: `row`, alignItems: 'flex-end', justifyContent: 'center' }}>
       <Button
         color={colors.secundary}
         onPress={logout}
