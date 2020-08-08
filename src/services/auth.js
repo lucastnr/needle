19866725e31bfc9e64993bf85184abc4c6fa5c82
Lucas <tnrlucas@gmail.com>
2020-08-08 05:40:34 -0300

    version 0.1.5

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,17 @@
+import qs from 'querystring'
+
 export async function signIn(email, password) { // Função que requere os dados do usuário  
   try {
-    const response = await fetch(`https://needle-server.herokuapp.com/login/${email}/${password}`)
+    const response = await fetch('https://needle-server.herokuapp.com/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: qs.stringify({
+        email: email,
+        password: password,
+      })
+    })
     const json = await response.json()
     return json
   } catch (error) {
--- src/services/register.js
+++ src/services/register.js
@@ -1,11 +1,17 @@
+import qs from 'querystring'
+
 export async function register(cpf, email, password) {
   console.log(cpf + " " + email +" " + password)
+
   const response = await fetch('https://needle-server.herokuapp.com/register', {
     method: 'POST',
-    body: JSON.stringify({
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: qs.stringify({
       cpf: cpf,
       email: email,
-      password: password
+      password: password,
     })
   })
 }
